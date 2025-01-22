7a81ef8e50532a86998bb0f28fd39be68f5acb42
Marco Antonio Nastasi <mnastasi@gmail.com>
2025-01-22 09:33:32 +0100

    Added description to rules, default sg, ebs optimized

--- main.tf
+++ main.tf
@@ -11,6 +11,25 @@ resource "aws_vpc" "docker_playground_vpc" {
   )
 }
 
+# Create default security group that restricts all traffic
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.docker_playground_vpc.id
+
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Create a public subnet inside the VPC using the CIDR configured in subnet_prefix
 resource "aws_subnet" "docker_playground_public_subnet" {
   vpc_id     = aws_vpc.docker_playground_vpc.id
@@ -39,11 +58,14 @@ resource "aws_security_group" "docker_playground_sg" {
 }
 
 # Create Ingress Rules for Security Group
-resource "aws_vpc_security_group_ingress_rule" "ingress_rules" {
-  count             = length(var.allowed_ports)
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule" {
+  count = length(var.allowed_ports)
+
+  description       = "Allow from Owner IP address to port ${var.allowed_ports[count.index]}"
   security_group_id = aws_security_group.docker_playground_sg.id
 
-  cidr_ipv4   = var.my_own_public_ip
+  cidr_ipv4 = var.my_own_public_ip
+
   # Allow traffic to ports in variable "allowed ports" 
   from_port   = var.allowed_ports[count.index]
   ip_protocol = "tcp"
@@ -58,11 +80,12 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_rules" {
 }
 
 # Create Egress Rules for the Security Group
-resource "aws_vpc_security_group_egress_rule" "example" {
+resource "aws_vpc_security_group_egress_rule" "egress_rule" {
+  description       = "Allow all outgoing traffic"
   security_group_id = aws_security_group.docker_playground_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  
+  cidr_ipv4 = "0.0.0.0/0"
+
   # Allow all outgoing traffic from the EC2 instance
   ip_protocol = "-1"
 
@@ -169,14 +192,20 @@ resource "aws_iam_role_policy_attachment" "amazon_ssm_managed_instance_core" {
 
 # Create EC2 instance using the user data script
 resource "aws_instance" "docker_playground" {
-  ami                         = data.aws_ami.ubuntu_2204.id
-  instance_type               = var.instance_type
+  ami           = data.aws_ami.ubuntu_2204.id
+  instance_type = var.instance_type
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
 
   # EC2 instance will have a public IP in the most basic deployment of the app
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.docker_playground_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.docker_playground_sg.id]
   iam_instance_profile        = aws_iam_instance_profile.docker_playground.name
+  ebs_optimized               = true
 
   tags = merge(
     local.tags,
@@ -195,11 +224,6 @@ resource "aws_instance" "docker_playground" {
   }
 
   user_data = file("scripts/boot_script.sh")
-}
-
-# Enforce IMDSv2
-resource "aws_ec2_instance_metadata_defaults" "enforce-imdsv2" {
-  http_tokens                 = "required"
 }
 
 # Define tags for all resources
